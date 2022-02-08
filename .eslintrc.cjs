1dfe7cd079ce51a7a34a83dbe56078c9ccf80384
ToshiKajitsu <toshikajitsu@gmail.com>
2022-02-08 19:56:39 +0400

    git actions

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+	"settings": {
+		"jest": {
+			"version": 27
+		}
+	},
+	'plugins': ['jest'],
+	'env': {
+		'node': true,
+		'es2021': true
+	},
+	'extends': ['airbnb-base', 'plugin:jest/recommended'],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'rules': {
+		'no-console': [
+			0
+		],
+		'import/extensions': [
+			"error",
+			"ignorePackages",
+			{
+				"js": "always",
+			}
+		]
+	}
+}
